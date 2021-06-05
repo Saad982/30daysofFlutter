f7710df808052d477cfd98d4efa6d0fbedf7e9e4
Saad <Saad982@gmail.com>
2021-06-05 16:00:45 +0500

    Day2Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:eght_ours/home_page.dart';
+import 'package:eght_ours/Package/login_page.dart';
+
+import 'Package/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,7 +13,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:HomePage(),
+
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+      ),
+      routes: {
+    "/" : (context)=>LoinPage(),
+        "/home_page" : (context)=>HomePage(),
+        "/login_page" : (context)=>LoinPage()
+      },
+
     );
   }
 }
