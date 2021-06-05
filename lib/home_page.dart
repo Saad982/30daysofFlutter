9c8e4a1039ea00f9269f608e130629eced24fcaa
Saad <Saad982@gmail.com>
2021-06-05 14:13:51 +0500

    Day2Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+int age=8;
+String name ="SAAd";
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to ${age} days of flutter $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+
+  }
+}
